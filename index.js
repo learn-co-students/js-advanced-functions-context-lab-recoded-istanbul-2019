ce12bdc79c34a2ff08d36fee9c5c134347b89ca0
Oktay Ibis <oktayibis@gmail.com>
2020-02-09 21:28:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,95 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord(employeeArray){
+
+    const [firstName, familyName, title, payPerHour] = employeeArray;
+
+    return Object.assign({}, 
+        { firstName }, 
+        { familyName }, 
+        { title }, 
+        { payPerHour }, 
+        { timeInEvents: [] }, 
+        { timeOutEvents: [] })
+}
+
+function createEmployees(employeeArray){
+    return employeeArray.map(e => {
+        return createEmployeeRecord(e);
+    })
+}
+
+function createEmployeeRecords(employeesArray){
+    return employeesArray.map(e => {
+        return createEmployeeRecord(e);
+    })
+}
+
+function createTimeInEvent(dateStamp){
+    let [date, clock] = dateStamp.split(' ');
+    let event = {
+        type : 'TimeIn',
+        hour : Number(clock),
+        date : date
+    }
+
+     this.timeInEvents.push(event)
+     return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let [date, clock] = dateStamp.split(' ');
+    let event = {
+        type : 'TimeOut',
+        hour : Number(clock),
+        date : date
+    }
+    this.timeOutEvents.push(event);
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    //find date of time in 
+    let timeIn = this.timeInEvents.find(f => {
+        return f.date === dateStamp
+    })
+    //find date of time out
+    let timeOut = this.timeOutEvents.find(f => {
+        return f.date === dateStamp
+    })
+
+    //diffirance between 
+    let workedHors = timeOut.hour - timeIn.hour;
+
+    return  workedHors / 100;
+
+}
+
+
+function wagesEarnedOnDate(dateStamp){
+    return (hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour)
+}
+
+function findEmployeebyFirstName(employeeArray, firstName){
+    return employeeArray.find(e=>{
+        return e.firstName === firstName;
+    })
+}
+
+function calculatePayroll(employeeArray){
+    let wages = employeeArray.map( e => {
+        return allWagesFor.call(e);
+    })
+
+    return wages.reduce((memo, value)=>{
+        return memo + value;
+    })
+
+  
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +110,8 @@ let allWagesFor = function () {
 
     return payable
 }
+
+
+
+
+
