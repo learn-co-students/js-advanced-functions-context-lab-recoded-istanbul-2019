9b68286b1dda4de36bf53532f441862f06978350
lobana alarabi <loubanaeng@gmail.com>
2020-02-10 19:07:13 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,90 @@
  for you to use if you need it!
  */
 
+
+function createEmployeeRecord(arr){
+  let employee={};
+  employee["firstName"]=arr[0];
+   employee["familyName"]=arr[1];
+   employee["title"]=arr[2];
+   employee["payPerHour"]=arr[3];
+   employee["timeInEvents"]=[];
+   employee["timeOutEvents"]=[];
+   
+  return employee;
+  
+}
+
+
+function createEmployeeRecords(employees){
+  return employees.map(emp =>createEmployeeRecord(emp));
+}
+
+function createEmployees(employees){
+  return employees.map(emp =>createEmployeeRecord(emp));
+}
+
+function createTimeInEvent(date){
+  let hh=date.split(" ");
+  let newevent={};
+  
+  newevent["type"]="TimeIn";
+  
+  newevent["date"]=hh[0];
+  newevent["hour"]=parseInt(hh[1]);
+  // emp["timeInEvents"].push(newevent);
+  this.timeInEvents.push(newevent);
+  
+  return this;
+}
+
+
+function createTimeOutEvent(date){
+  let hh=date.split(" ");
+  let newevent={};
+  
+  newevent["type"]="TimeOut";
+  newevent["date"]=hh[0];
+  newevent["hour"]=parseInt(hh[1]);
+  this.timeOutEvents.push(newevent);
+  
+  return this;
+}
+
+function hoursWorkedOnDate(date){
+  let nHour=0;
+  let i;
+  let found=false;
+  for(i=0;i<this.timeInEvents.length;i++)
+  {
+    if(this.timeInEvents[i]["date"]==date){
+      found=true;
+      break;
+    }
+  }
+  if(found)
+  {
+    nHour=Math.abs(this.timeInEvents[i]["hour"]-this.timeOutEvents[i]["hour"])/100
+    found=false;
+  }
+  return nHour;
+}
+
+function wagesEarnedOnDate(date){
+return hoursWorkedOnDate.call(this,date) * this.payPerHour;
+}
+
+function findEmployeebyFirstName(srcArray,fName){
+  for(let i=0;i<srcArray.length;i++)
+  {
+    if(srcArray[i]["firstName"]==fName)
+    {
+      return srcArray[i]
+    }
+  }
+  return undefined;
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +103,25 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+} 
+// function calculatePayroll(array){
+// return array.reduce((memoo,emp) => {
+//   return memoo += allWagesFor.call(this)},0)
+  
+// }
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.map(element => allWagesFor.call(element)).reduce(function(total, element) {
+        return total + element
+    }, 0)
 }
+ const csvTimesOut = [
+        ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
+        ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
+        ["Natalia", ["2018-01-03 2300", "2018-01-05 2300", "2018-01-03 2300"]],
+        ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
+        ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
+        ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
+      ]
+
+calculatePayroll(csvTimesOut)
