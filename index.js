4eae2a27590cee0e43815e0c2d79663f0c67852e
mmelihdogan <doganmmelih@gmail.com>
2020-02-18 17:54:07 +0300

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,60 @@
  for you to use if you need it!
  */
 
+let createEmployeeRecord = function (employee) {
+    let employeeRecord = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+
+    return employeeRecord;
+}
+
+let createEmployeeRecords = function (employee) {
+    return employee.map(i => createEmployeeRecord(i));
+}
+
+let createEmployees = function (employee) {
+    let newEmployeeRecord = [];
+    for (let e in employee) {
+        newEmployeeRecord.push(createEmployeeRecord(employee[e]))
+    }
+    return newEmployeeRecord;
+}
+
+let createTimeInEvent = function (date) {
+    let time = date.split(' ');
+    this.timeInEvents.push({ type: 'TimeIn',
+                    hour: parseInt(time[1]),
+                    date: time[0]
+                    });
+    return this;
+}
+
+let createTimeOutEvent = function (date) {
+    let time = date.split(' ');
+    this.timeOutEvents.push({ type: 'TimeOut',
+                    hour: parseInt(time[1]),
+                    date: time[0]
+                    });
+    return this;
+}
+
+let hoursWorkedOnDate = function (date) {
+    let timeIn = this.timeInEvents.find(event => event.date === date);
+    let timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+let wagesEarnedOnDate = function (date) {
+    let hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour; 
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,3 +74,13 @@ let allWagesFor = function () {
 
     return payable
 }
+
+
+let calculatePayroll = function (employee) {
+    return employee.reduce((total, element) => total + allWagesFor.call(element), 0)
+} 
+
+let findEmployeebyFirstName = function (collection, firstNameString) {
+    return collection.find(m => (m.firstName === firstNameString));
+}
+
