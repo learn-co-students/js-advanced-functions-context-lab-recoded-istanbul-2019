f841190a0c18967c9aac63c202dd611d39c5c9c3
zerozeynep <zeynep-demirtas@windowslive.com>
2020-02-23 00:37:45 +0200

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,88 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(employeeArr){
+    let employeeRec = {
+        'firstName':employeeArr[0],
+        'familyName':employeeArr[1],
+        'title':employeeArr[2],
+        'payPerHour':employeeArr[3],
+        'timeInEvents':[],
+        'timeOutEvents':[]
+    }
+    return employeeRec
+}
+
+
+let createEmployees = function(employeeArrs){
+    return employeeArrs.map(employee =>createEmployeeRecord(employee))
+}
+
+let createTimeInEvent = function(dateStamp){
+    
+    let date = dateStamp.split(' ')[0]
+    let hour = parseInt(dateStamp.split(' ')[1])
+
+    let obj = {
+        type: 'TimeIn',
+        hour,
+        date
+    }
+
+    this.timeInEvents.push(obj)
+
+    return this
+
+}
+
+
+let createTimeOutEvent = function(dateStamp){
+    let date = dateStamp.split(' ')[0]
+    let hour = parseInt(dateStamp.split(' ')[1])
+
+    let obj = {
+        type: 'TimeOut',
+        hour, 
+        date
+    }
+
+    this.timeOutEvents.push(obj)
+
+    return this
+
+}
+
+
+let hoursWorkedOnDate =  function(dateStamp){
+    let timeIn = this.timeInEvents.find(event=>event.date===dateStamp)
+    let timeOut = this.timeOutEvents.find(event=>event.date===dateStamp)
+
+    return (timeOut.hour-timeIn.hour)/100
+    
+}
+
+let wagesEarnedOnDate = function(dateStamp){
+   return  hoursWorkedOnDate.call(this,dateStamp)*this.payPerHour
+}
+
+let calculatePayroll = function(employeeArrs){
+    return employeeArrs.reduce(((total,current)=> total + allWagesFor.call(current)),0)
+}
+
+let createEmployeeRecords = function(employeeArrs){
+    return employeeArrs.map(employee=>createEmployeeRecord(employee))
+    
+}
+
+let findEmployeebyFirstName = function(employeeRecArr,firstN){
+    return employeeRecArr.find(employee=>employee.firstName === firstN)
+}
+
+
+
+
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +103,5 @@ let allWagesFor = function () {
 
     return payable
 }
+
+
