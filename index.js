9b8f6bfc1b9142da8f5b2611c70f69a1b9e1f8e1
mira_arar <mira.ar230@gmail.com>
2020-05-05 03:51:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array){
+    let employeeInfo=[...array]
+    let employee={
+        firstName : employeeInfo[0],
+        familyName : employeeInfo[1],
+        title : employeeInfo[2],
+        payPerHour : employeeInfo[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+    return employee;
+}
+
+function createEmployeeRecords(arrayOfArrays){
+    let arrayOfObjects=[]
+   arrayOfObjects = arrayOfArrays.map((element)=>{
+    return createEmployeeRecord(element)
+   })
+    return arrayOfObjects;
+}
+
+function createTimeInEvent(date){
+    console.log(date);
+    const timeInObject={
+        type:"TimeIn",
+        date: date.split(" ")[0],
+        hour: parseInt(date.split(" ")[1])
+    }
+    this.timeInEvents.push(timeInObject);
+    return  this;
+}
+
+function createTimeOutEvent(date){
+    const timeOutObject={
+        type:"TimeOut",
+        date: date.split(" ")[0],
+        hour: parseInt(date.split(" ")[1])
+    }
+    this.timeOutEvents.push(timeOutObject);
+    return  this
+}
+
+function hoursWorkedOnDate(date){
+    let hoursWorked;
+    let index;
+    let timeArray= this.timeOutEvents;
+    for(const element of timeArray){
+        if(element.date===date){index= timeArray.indexOf(element)}
+    }
+        hoursWorked= timeArray[index].hour- this.timeInEvents[index].hour;
+    return hoursWorked/100;
+}
+
+function wagesEarnedOnDate(date){
+    return  hoursWorkedOnDate.call(this ,date) * this.payPerHour;
+   
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,3 +77,14 @@ let allWagesFor = function () {
 
     return payable
 }
+function findEmployeebyFirstName(srcArray,firstName){
+    let matchingRecord= srcArray.find(element=> element.firstName === firstName)
+    return matchingRecord;
+}
+
+function calculatePayroll(employeeRecords){
+    return employeeRecords.reduce((acc, employeeRecord) => {
+        return acc + allWagesFor.call(employeeRecord)}, 0)
+
+}
+
