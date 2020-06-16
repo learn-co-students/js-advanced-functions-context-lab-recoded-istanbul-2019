2e5e9814d771abab190c022806b8b83591ac2951
Dara <dara.dandashi@gmail.com>
2020-06-16 22:58:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    
+    let obj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+
+    }
+    return obj 
+}
+
+function createEmployeeRecords(recor) {
+    return recor.map(curr => createEmployeeRecord(curr))
+}
+
+
+function createEmployees(records) {
+    return records.map(record => createEmployeeRecord(record))
+ 
+ }
+ 
+ function createTimeInEvent(str) {
+     const [date, hour] = str.split(' ');
+     this.timeInEvents.push({
+         type: 'TimeIn',
+         hour: parseInt(hour, 10),
+         date: date,
+     });
+     return this;
+ }
+
+ function createTimeOutEvent(str) {
+    const [date, hour] = str.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date: date,
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(str) {
+    const timeInEvent = this.timeInEvents.find(curr => curr.date === str);
+    const timeOutEvent = this.timeOutEvents.find(curr => curr.date === str);
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(str) {
+    let payOwed = hoursWorkedOnDate.call(this, str) * this.payPerHour;
+    return parseFloat(payOwed.toString());
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
